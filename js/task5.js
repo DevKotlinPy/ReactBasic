ac3c5e1e0300877e9365c098c54e1a0f7f20a93a
DevKotlinPy <arielmarin39@outlook.com>
2024-07-18 21:00:07 -0600

    nueva rama

--- js/task5.js
+++ js/task5.js
@@ -26,11 +26,32 @@ const empleado2 = cliente.map(function(item){
 console.log(empleado2)
 
 
-const lenguaje =["VC",'C#','JS','React.JS',"DevExpress",'Java',"Kotlin","Xamarin","PHP","Angular"]
+const lenguaje =["VB",'C#','JS','React.JS',"DevExpress",'Java',"Kotlin","Xamarin","PHP","Angular"]
 console.log(lenguaje.length)
 console.log(lenguaje[3])
 
 console.log("Distructuring array")
- const [,,,,,,kotlin] = lenguaje
+ const [,,,,,,kotlin,,,Angular] = lenguaje
 
  console.log(kotlin)
+ console.log(Angular)
+
+ lenguaje.forEach(element => {
+    console.log(element)
+ });
+
+ console.log("-◘◘-MAP-◘◘-")
+ const tecnologias = lenguaje.map(function(elemen){
+    
+    if(elemen === 'VB'){
+        return 'Visual Basic 2024'
+    }else{
+        return elemen
+    }
+ });
+console.table(tecnologias)
+
+console.log("-◘◘-FOR OF-◘◘-")
+ for( let i of lenguaje){
+    console.log(i)
+ }
